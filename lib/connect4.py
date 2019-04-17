eb6f510e2439520a70f19fbf25de903daabf6a45
“Brenda” <odonnell.brenda@gmail.com>
2019-04-17 12:31:57 +0100

    Mock test and simplest_win tests, works in progress

--- lib/connect4.py
+++ lib/connect4.py
@@ -1,8 +1,23 @@
 class Connect4():
 
-    def start():
-        return
+    def __init__(self):
+        self.playing_board = [["-,-,-,-,-,-,-"],
+            ["-,-,-,-,-,-,-"],
+            ["-,-,-,-,-,-,-"],
+            ["-,-,-,-,-,-,-"],
+            ["-,-,-,-,-,-,-"],
+            ["-,-,-,-,-,-,-"],
+            ["-,-,-,-,-,-,-"]
+    ]  
 
-    def move(player, column):
+    def start(self):
+        return self.playing_board
+
+    def move(self, player, column):
+        print('Enter your move: ')
+        column = input()
         return True
 # comment for commit
+
+c4 = Connect4()
+c4.start()
--- test/connect4_test.py
+++ test/connect4_test.py
@@ -5,14 +5,38 @@ import os
 #
 sys.path.insert(0, os.path.abspath("lib"))
 
-from connect4 import Connect4 as c4# from connect4 import Connect4
+import connect4 as c4 # from connect4 import Connect4
+
+# def test_connect4(c4):
 
 def test_simplest_win():
-    c4.start()
-    c4.move(1, 6) # c4.move(player, column number)
-    c4.move(2, 7)
-    c4.move(1, 6)
-    c4.move(2, 2)
-    c4.move(1, 6)
-    c4.move(2, 7)
-    assert c4.move(1, 6) == True
+    input_values = [6, 7, 6, 7, 6, 7, 6]
+    output = []
+
+    def mock_input(s):
+        return input_values.pop(0)
+
+    c4.Connect4.input = mock_input
+    c4.Connect4.print = lambda s : output.append(s)
+    c4.Connect4.start()
+
+    assert output.count(["Player 1 wins"]) == 1
+
+def test_c4_start(self):
+    mock_board = [["-,-,-,-,-,-,-"],
+            ["-,-,-,-,-,-,-"],
+            ["-,-,-,-,-,-,-"],
+            ["-,-,-,-,-,-,-"],
+            ["-,-,-,-,-,-,-"],
+            ["-,-,-,-,-,-,-"],
+            ["-,-,-,-,-,-,-"]
+    ]   
+    assert c4.Connect4.start().playing_board == mock_board 
+    # assert c4.start.board.playing_board.count == 1 
+    # c4.move(1, 6) # c4.move(player, column number)
+    # c4.move(2, 7)
+    # c4.move(1, 6)
+    # c4.move(2, 2)
+    # c4.move(1, 6)
+    # c4.move(2, 7)
+    # assert c4.move(1, 6) == True
