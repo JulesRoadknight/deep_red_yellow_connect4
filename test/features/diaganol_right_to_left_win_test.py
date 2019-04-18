84a139344ff75d04caf837f07f9f23d327bf3fd0
“Brenda” <odonnell.brenda@gmail.com>
2019-04-18 15:57:19 +0100

    Wins on a diagonal from bottom right to middle with winning piece two from top

--- test/features/diaganol_right_to_left_win_test.py
+++ test/features/diaganol_right_to_left_win_test.py
@@ -37,3 +37,19 @@ def test_diaganol_win_for_player1_from_bottom_right_corner_to_middle_with_last_p
     game.start()
 
     assert output.count("Player 1 wins") == 1
+
+def test_diaganol_win_for_player1_from_bottom_right_corner_to_middle_with_last_piece_2_from_top():
+    output = []
+    input_values = [7, 6, 5, 5, 5, 4, 4, 4, 4, 3, 6]
+
+    def mock_input(s):
+        output.append(s)
+        return input_values.pop(0)
+
+    c4.input = mock_input
+    c4.print = lambda s : output.append(s)
+
+    game = c4.Connect4()
+    game.start()
+
+    assert output.count("Player 1 wins") == 1
