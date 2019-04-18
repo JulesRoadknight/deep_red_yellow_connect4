a01a50ce50f65ea6c89f25e9f7c73019ba2ee38d
“Brenda” <odonnell.brenda@gmail.com>
2019-04-18 15:12:26 +0100

    Checks win on diagonal from bottom left to the middle with last move one in from the start

--- test/features/diaganol_win_test.py
+++ test/features/diaganol_win_test.py
@@ -37,3 +37,19 @@ def test_diaganol_win_for_player1_from_2nd_collumn_row_2_last_move_in_1_from_top
     game.start()
 
     assert output.count("Player 1 wins") == 1
+
+def test_diaganol_win_for_player1_from_bottom_left_corner_to_last_move_in_2_from_top_of_diagonal():
+    output = []
+    input_values = [1, 2, 3, 3, 3, 4, 4, 4, 4, 5, 2]
+
+    def mock_input(s):
+        output.append(s)
+        return input_values.pop(0)
+
+    c4.input = mock_input
+    c4.print = lambda s : output.append(s)
+
+    game = c4.Connect4()
+    game.start()
+
+    assert output.count("Player 1 wins") == 1
