fd47699434e994b99939539cf238c3735155f242
Steve Kahn <steve@kahn.org.uk>
2019-04-18 15:17:02 +0100

    Diaganol from bottom left to middle wins with last peice bottom left

--- test/features/diaganol_win_test.py
+++ test/features/diaganol_win_test.py
@@ -53,3 +53,19 @@ def test_diaganol_win_for_player1_from_bottom_left_corner_to_last_move_in_2_from
     game.start()
 
     assert output.count("Player 1 wins") == 1
+
+def test_diaganol_win_for_player1_from_bottom_left_corner_to_middle_last_move_bottom_left():
+    output = []
+    input_values = [3, 2, 2, 3, 3, 4, 4, 4, 4, 5, 1]
+
+    def mock_input(s):
+        output.append(s)
+        return input_values.pop(0)
+
+    c4.input = mock_input
+    c4.print = lambda s : output.append(s)
+
+    game = c4.Connect4()
+    game.start()
+
+    assert output.count("Player 1 wins") == 1
