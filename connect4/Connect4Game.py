9b25710e52a0e2d2ae8eccb0c613d69c9d4b7bc8
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-04-24 16:04:55 +0100

    l

--- connect4/Connect4Game.py
+++ connect4/Connect4Game.py
@@ -14,11 +14,20 @@ class Connect4Game(Connect4):
     def __init__(self, height=0, width=0, win_length=0, np_pieces=0):
         Connect4.__init__(self)
         self.base_board = board.Board()
-        print(self.base_board)
-        self.base_board_height = height
+        # print(self.base_board)
+        self.base_board_height = len(self.base_board.show())
+        print("This is the game board height:")
         print(self.base_board_height)
-        self.base_board_width = width
-        self.base_board_win_length = win_length
+        self.base_board_width = len(self.base_board.show()[0])
+        print("This is the game board width:")
+        print(self.base_board_width)
+        # print(self.base_board.game_board[0])
+        # print("This is the board height:")
+        # print(self.base_board_height)
+        # self.base_board_width = len(self.base_board.game_board[0][0])
+        # print("This is the board width:")
+        # print(self.base_board_width)
+        # self.base_board_win_length = win_length
         self.base_board_np_pieces = np_pieces
 
     def getInitBoard(self):
