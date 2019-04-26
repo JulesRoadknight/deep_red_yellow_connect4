7448d526d96e67e49d17a924b51509fe8a4e0c4b
SurekaStoeckigt <sureka.stoeckigt@gmail.com>
2019-04-26 12:22:06 +0100

    python2

--- alpha-zero/connect4.py
+++ alpha-zero/connect4.py
@@ -62,8 +62,9 @@ class Connect4():
         return self.game_board.moves.check_win()
 
     def players(self):
-        if input("Player 1 is a Human or AI?") == "Human" : self.player1 = human.Human(self.moves)
-        if input("Player 2 is a Human or AI?") == "Human" : self.player2 = human.Human(self.moves)
+        input_func = input
+        if input_func("Player 1 is a Human or AI?") == "Human" : self.player1 = human.Human(self.moves)   
+        if input_func("Player 1 is a Human or AI?") == "Human" : self.player2 = human.Human(self.moves)
         return
 
 if __name__ == "__main__":
