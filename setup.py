a5802766fc0dacb1fe10a1ca382fce4c85916431
Jimjule <roadknightj@gmail.com>
2019-04-30 16:25:07 +0100

    Converted running instructions into an executable file

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-In the console, run:
-
-python
-
 import os
 import sys
 sys.path.insert(0, os.path.abspath('gym-connect4/gym_connect4/envs/lib'))
@@ -10,6 +6,3 @@ import connect4 as c4
 game = c4.Connect4()
 
 game.start()
-
-
-  Then, type numbers 0-6 to make moves, alternating between players.
